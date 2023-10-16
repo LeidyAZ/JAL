f7f0fab1e886cb766230ffa8354507beb70881c8
leidy <ljalomia3@soy.sena.edu.co>
2023-10-16 11:45:39 -0500

    2.0 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './estructura/header/header.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+import { RouterComponent } from './modulos/router/router.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent,
+    RouterComponent
   ],
   imports: [
     BrowserModule,
